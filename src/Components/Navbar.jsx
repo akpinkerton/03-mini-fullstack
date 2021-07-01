38b00e28ae6f39e3c59b3e0b94801c4c05ccb680
akpinkerton <akpinkerton@gmail.com>
2021-07-01 14:54:09 -0700

    cheesed nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
+import cheese from '../Local-storage/cheese.png'
 
 const Navbar = () => {
     return (
@@ -7,8 +7,14 @@ const Navbar = () => {
         <div class="container-fluid">
           <h1 className='navbar-brand'>CODING CHEETSHEETS🧀</h1>
           <ul class="navbar-nav">
-            <li className="nav-item"> <Link to="/" className="nav-link">Home</Link></li>
-            <li className="nav-item"> <Link to="/inputcheats" className="nav-link">Input Cheats</Link></li>
+            <li className="nav-item"> <Link to="/" className="nav-link">
+                <span><img className="photo" src={cheese}/></span>
+                <span>All of your cheets</span>
+            </Link></li>
+            <li className="nav-item"> <Link to="/inputcheats" className="nav-link">
+                <span><img src="https://img.icons8.com/emoji/50/000000/cheese-wedge-emoji.png"/></span>
+                <span>Add a cheet</span>
+            </Link></li>
           </ul>
         </div>
       </nav>
